6c1fe10932aaada87e5f48eb49094598e31dc7a5
Marcus <marcusicaromc@gmail.com>
2024-08-29 21:01:31 -0300

    implement backend errors component

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,20 +1,20 @@
-import { Component } from "@angular/core";
-import { FormBuilder, Validators, ReactiveFormsModule } from "@angular/forms";
-import { authActions } from "../../store/action";
-import { Store } from "@ngrx/store";
-import { RegisterRequestInterface } from "../../types/registerRequest.interface";
-import { RouterLink } from "@angular/router";
-import { AuthStateInterface } from "../../types/auth.state.interface";
 import { CommonModule } from "@angular/common";
-import { selectIsSubmitting } from "../../store/reducers";
-import { AuthService } from "../../services/auth.service";
+import { Component } from "@angular/core";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
+import { RouterLink } from "@angular/router";
+import { Store } from "@ngrx/store";
+import { combineLatest } from "rxjs";
+import { BackendErrorMessages } from "src/app/shared/components/backendErrorMessages.component";
+import { authActions } from "../../store/action";
+import { selectIsSubmitting, selectValidationErrors } from "../../store/reducers";
+import { RegisterRequestInterface } from "../../types/registerRequest.interface";
 
 @Component({
     selector: 'mc-register',
     templateUrl: './register.component.html',
     standalone: true,
     // CommonModule prevents No pipe found with name 'async'
-    imports: [ReactiveFormsModule, RouterLink, CommonModule]
+    imports: [ReactiveFormsModule, RouterLink, CommonModule, BackendErrorMessages]
 })
 
 export class RegisterComponent {
@@ -24,7 +24,10 @@ export class RegisterComponent {
         password: ['', Validators.required],
     })
 
-    isSubmitting$ = this.store.select(selectIsSubmitting)
+    data$ = combineLatest({
+        isSubmitting: this.store.select(selectIsSubmitting),
+        backendErrors: this.store.select(selectValidationErrors)
+    })
 
     constructor(private fb: FormBuilder, private store: Store){}
 
